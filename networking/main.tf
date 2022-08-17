de8e420bdb97a91abd6eef4f72b8ef9d257b1aad
bfreeland <62081513+bfreeland56@users.noreply.github.com>
2022-08-16 21:33:44 -0400

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -112,14 +112,14 @@ resource "aws_security_group" "project_public_sg" {
   vpc_id      = aws_vpc.project_vpc.id
 }  
 
- ingress {
+ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.access_ip]
  }
 
- egress {
+egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -131,28 +131,28 @@ resource "aws_security_group" "project_private_sg" {
   vpc_id      = aws_vpc.project_vpc.id
 }
 
- ingress {
+ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
     security_groups = [aws_security_group.project_public_sg.id]
   }
 
- egress {
+egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+ingress {
     from_port       = 80
     to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.project_web_sg.id]
   }
 
- egress {
+egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -165,14 +165,14 @@ resource "aws_security_group" "project_web_sg" {
   vpc_id      = aws_vpc.project_vpc.id
 }
 
-  ingress {
+ ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
+ egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
